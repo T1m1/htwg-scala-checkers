627581d158ae24523bd1b118ed7172c81679c3a6
Steffen Gorenflo <SteffenGorenflo@gmail.com>
2016-10-13 18:26:13 +0200

    playing around

--- src/main/scala/de/htwg/se/checkers/Hello.scala
+++ src/main/scala/de/htwg/se/checkers/Hello.scala
@@ -4,7 +4,9 @@ import de.htwg.se.checkers.model.Student
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    val student = Student("Your Name")
+    val student = Student("Timi")
+
     println("Hello, " + student.name)
+    println(s"Hello, ${student.name}") // string interpolation: http://docs.scala-lang.org/overviews/core/string-interpolation.html
   }
 }
