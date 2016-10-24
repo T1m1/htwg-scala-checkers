f7a379d13ea30ffa7c092053a3f536cd12008967
Timi <ruprecht.t@gmx.de>
2016-10-24 12:16:49 +0200

    add playfield

--- src/main/scala/de/htwg/se/checkers/model/PlayField.scala
+++ src/main/scala/de/htwg/se/checkers/model/PlayField.scala
@@ -1,5 +1,19 @@
 package de.htwg.se.checkers.model
 
 case class PlayField(size: Int) {
+  val field = Array.ofDim[Any](size, size)
+
+  init()
+
+  def init(): Unit = {
+    val numberOfRowsPerPlayer = getNumberOfRowsPerPlayer(size)
+    // Feld Zusammenbauen
+  }
+
+  // TODO
+  private def getNumberOfRowsPerPlayer(number: Int): Int = {
+    number / 2 - 0.5.toInt
+  }
+
 
 }
