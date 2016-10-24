489f0010319a6646b7daed159da9abc809f905cd
Timi <ruprecht.t@gmx.de>
2016-10-24 14:56:08 +0200

    calculate number of rows for player

--- src/main/scala/de/htwg/se/checkers/model/PlayField.scala
+++ src/main/scala/de/htwg/se/checkers/model/PlayField.scala
@@ -2,17 +2,14 @@ package de.htwg.se.checkers.model
 
 case class PlayField(size: Int) {
   val field = Array.ofDim[Any](size, size)
+  val numberOfRowsPerPlayer = calculateNumberOfRowsPerPlayer(size)
 
-  init()
 
-  def init(): Unit = {
-    val numberOfRowsPerPlayer = getNumberOfRowsPerPlayer(size)
-    // Feld Zusammenbauen
-  }
-
-  // TODO
-  private def getNumberOfRowsPerPlayer(number: Int): Int = {
-    number / 2 - 0.5.toInt
+  def calculateNumberOfRowsPerPlayer(number: Int): Int = {
+    if (number == 3) {
+      1
+    }
+    (Math.ceil(number / 2) - 0.5).toInt
   }
 
 
