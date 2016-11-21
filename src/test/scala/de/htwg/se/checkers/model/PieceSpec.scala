caf9616c9ef6fc95ada452f50aa92f60fa49ac5e
Timi <ruprecht.t@gmx.de>
2016-11-21 09:40:18 +0100

    refactor tests

--- src/test/scala/de/htwg/se/checkers/model/PieceSpec.scala
+++ src/test/scala/de/htwg/se/checkers/model/PieceSpec.scala
@@ -7,7 +7,8 @@ class PieceSpec extends WordSpec with Matchers {
   "A new default Piece" should {
     val x = 4
     val y = 4
-    val piece = new Piece(x, y)
+    val color = 0;
+    val piece = new Piece(x, y, 0)
 
     "be not a checkers" in {
       piece.checkers should be(false)
