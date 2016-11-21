9bb8a2d67a72d77e445733fee2a327249b45f141
Timi <ruprecht.t@gmx.de>
2016-11-21 09:39:18 +0100

    add main

---
+++ src/main/scala/de/htwg/se/checkers/Checkers.scala
@@ -0,0 +1,18 @@
+package de.htwg.se.checkers
+
+import de.htwg.se.checkers.controller.CheckersController
+import de.htwg.se.checkers.model.PlayField
+import de.htwg.se.checkers.view.Tui
+
+object Checkers {
+
+  // TODO inject size of playfield
+  val playField = new PlayField(8)
+  val ctr = new CheckersController(playField)
+  val tui = new Tui(ctr)
+
+  def main(args: Array[String]): Unit = {
+    println(s"Press 's' to start the game: ")
+    while (tui.processInputLine(scala.io.StdIn.readLine())) {}
+  }
+}
