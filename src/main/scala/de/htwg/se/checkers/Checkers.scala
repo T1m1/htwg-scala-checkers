9ef6e73fb36062a37ef848e4f5468c8386ca981a
Timi <ruprecht.t@gmx.de>
2017-01-01 18:28:25 +0100

    create actors

--- src/main/scala/de/htwg/se/checkers/Checkers.scala
+++ src/main/scala/de/htwg/se/checkers/Checkers.scala
@@ -1,18 +1,19 @@
 package de.htwg.se.checkers
 
-import de.htwg.se.checkers.controller.CheckersController
-import de.htwg.se.checkers.model.Playfield
+import akka.actor.{ActorSystem, Props}
+import de.htwg.se.checkers.controller.{CheckersController, ControllerActor, CreateUI}
 import de.htwg.se.checkers.view.Tui
 
 object Checkers {
 
+  // inject
   implicit val bindingModule = CheckersConfiguration
 
-  val ctr = new CheckersController()(bindingModule)
-  val tui = new Tui(ctr)
-
   def main(args: Array[String]): Unit = {
-    println(s"Press 's' to start the game: ")
-    while (tui.processInputLine(scala.io.StdIn.readLine())) {}
+    val actorSystem = ActorSystem.create("checkers")
+    val controllerActor = actorSystem.actorOf(Props(new ControllerActor()))
+
+    // tui
+    actorSystem.actorOf(Props(new Tui(controllerActor)))
   }
 }
