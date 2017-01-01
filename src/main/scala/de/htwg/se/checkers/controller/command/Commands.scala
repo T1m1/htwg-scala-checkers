cfde6046dd03f3c6a3e4d50003f3cc47dc92bc15
Timi <ruprecht.t@gmx.de>
2017-01-01 18:27:33 +0100

    implement different commands for actors

---
+++ src/main/scala/de/htwg/se/checkers/controller/command/Commands.scala
@@ -0,0 +1,14 @@
+package de.htwg.se.checkers.controller.command
+
+import de.htwg.se.checkers.model.api._
+
+
+trait Command {}
+
+case class SetPiece(start: Coord, end: Coord) extends Command
+
+case class QuitGame() extends Command
+
+case class NewGame() extends Command
+
+case class PrintInfo() extends Command
