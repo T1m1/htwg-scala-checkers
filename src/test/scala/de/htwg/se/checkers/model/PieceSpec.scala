3ad69e792decdbcbbef6d08d7f395009a85d40dc
Timi <ruprecht.t@gmx.de>
2017-01-16 14:22:32 +0100

    100% Testcoverage piece

--- src/test/scala/de/htwg/se/checkers/model/PieceSpec.scala
+++ src/test/scala/de/htwg/se/checkers/model/PieceSpec.scala
@@ -5,13 +5,19 @@ import org.scalatest._
 
 class PieceSpec extends WordSpec with Matchers {
 
-  "A new default Piece" should {
+  "A new black default Piece" should {
 
     val piece = new Piece(Colour.BLACK)
+
+    // call unapply to get 100% coverage for case classes
+    Piece.unapply(piece)
 
     "be not a checkers" in {
       piece.checkers should be(false)
     }
 
+    "should be black" in {
+      piece.colour should be(Colour.BLACK)
+    }
   }
 }
